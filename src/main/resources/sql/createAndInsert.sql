4b9c98de6d47a3ecdf782defe1c52108de0e8520
dani154 <daniel.klampfl@gmail.com>
2018-11-24 21:01:47 +0100

    Add database schema for match player [#30477]

--- src/main/resources/sql/createAndInsert.sql
+++ src/main/resources/sql/createAndInsert.sql
@@ -2,7 +2,28 @@ CREATE TABLE IF NOT EXISTS match
 (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   dateTime TIMESTAMP NOT NULL,
-  teamRedGoals INT DEFAULT 0 NOT NULL,
-  teamBlueGoals INT DEFAULT 0 NOT NUll,
-  teamSize int NOT NULL
+  teamRedGoals INT NOT NULL,
+  teamBlueGoals INT NOT NUll,
+  teamSize INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS player
+(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name TEXT NOT NULL,
+  team INT NOT NULL,
+  score INT NOT NULL,
+  goals INT NOT NULL,
+  assists INT NOT NULL,
+  saves INT NOT NULL,
+  shots INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS playerInMatch
+(
+  playerid BIGINT,
+  matchid  BIGINT,
+  FOREIGN KEY (playerid) REFERENCES player(id),
+  FOREIGN KEY (matchid) REFERENCES match(id),
+  PRIMARY KEY (playerid,matchid)
 );
