08f237b0a58d1b32a734fec39ff7e2893135dc10
Bernhard Bayer <e1228892@student.tuwien.ac.at>
2018-12-01 20:49:21 +0100

    Main GUI added

---
+++ src/main/java/at/ac/tuwien/sepm/assignment/group/replay/ui/MainwindowController.java
@@ -0,0 +1,23 @@
+package at.ac.tuwien.sepm.assignment.group.replay.ui;
+
+import javafx.event.ActionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Component
+public class MainwindowController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+    public MainwindowController() {
+
+    }
+
+    public void onMatchdetailsButtonClicked(ActionEvent actionEvent) {
+
+    }
+}
