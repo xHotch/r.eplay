da3f605dd73908bbdf9ad4f20735a28beccf994c
01527619 <e1527619@student.tuwien.ac.at>
2018-12-02 17:49:12 +0100

    Implemented FileChooser to select replay files to upload [#30465]

--- src/main/java/at/ac/tuwien/sepm/assignment/group/replay/ui/MainwindowController.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/group/replay/ui/MainwindowController.java
@@ -1,9 +1,14 @@
 package at.ac.tuwien.sepm.assignment.group.replay.ui;
 
+import at.ac.tuwien.sepm.assignment.group.replay.exception.FileServiceException;
+import at.ac.tuwien.sepm.assignment.group.replay.service.ReplayService;
 import at.ac.tuwien.sepm.assignment.group.util.SpringFXMLLoader;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,11 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.util.concurrent.ExecutorService;
 
 /**
  * Main Window Controller.
+ *
  * @author Bernhard Bayer
  */
 @Component
@@ -29,6 +37,11 @@ public class MainwindowController {
     @Autowired
     MatchdetailController matchdetailController;
 
+    @Autowired
+    ExecutorService executorService;
+
+    @Autowired
+    ReplayService replayService;
 
     public MainwindowController() {
 
@@ -36,6 +49,7 @@ public class MainwindowController {
 
     /**
      * Opens a new Match Detail window.
+     *
      * @param actionEvent Actionevent from the button
      */
     public void onMatchdetailsButtonClicked(ActionEvent actionEvent) {
@@ -74,4 +88,61 @@ public class MainwindowController {
         //TODO
 
     }
+
+
+    /**
+     * Method that opens a FileChooser when the actionEvent occurs
+     * Lets the user chose .replay files
+     * <p>
+     * The chosen file gets sent to service layer, to parse it
+     *
+     * @param actionEvent Actionevent from the button
+     */
+    public void onUploadReplayButtonClicked(ActionEvent actionEvent) {
+        LOG.info("Image Chooser clicked");
+        LOG.trace("Called - onUploadMatchButtonClicked");
+
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterREPLAY = new FileChooser.ExtensionFilter("Replay files (*.replay)", "*.REPLAY");
+        fileChooser.getExtensionFilters().add(extFilterREPLAY);
+
+
+        //Show open file dialog
+        File inputFile = fileChooser.showOpenDialog(null);
+        if (inputFile == null) {
+            LOG.info("File selection cancelled");
+            return;
+        }
+
+        executorService.submit(() -> {
+            try {
+                replayService.parseReplayFileToJson(inputFile);
+
+            } catch (FileServiceException e) {
+                LOG.error("Cought File Service Exception");
+                Platform.runLater(() -> showErrorMessage(e.getMessage()));
+            }
+        });
+    }
+
+    /**
+     * Method to show a simple Error Alert to the user
+     *
+     * @param errorMessage The String containing the message displayed
+     */
+    private void showErrorMessage(String errorMessage) {
+
+        LOG.trace("Called - showErrorMessage");
+
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Wrong input error");
+        alert.setHeaderText("Error");
+        alert.setContentText(errorMessage);
+
+        alert.showAndWait();
+    }
+
+
 }
