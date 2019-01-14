f84e8709674d1f48ed84e26dcd7433306663f75c
Markus Kogelbauer <markus.kogelbauer@hotmail.com>
2019-01-14 19:59:46 +0100

    changed the parseService to read the playerData from the frames instead from the properties [#32050]

--- src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/PlayerInformationParser.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/PlayerInformationParser.java
@@ -1,6 +1,8 @@
 package at.ac.tuwien.sepm.assignment.group.replay.service.impl.parser;
 
 import at.ac.tuwien.sepm.assignment.group.replay.dto.MatchPlayerDTO;
+import at.ac.tuwien.sepm.assignment.group.replay.dto.PlayerDTO;
+import at.ac.tuwien.sepm.assignment.group.replay.dto.TeamSide;
 import at.ac.tuwien.sepm.assignment.group.replay.service.exception.FileServiceException;
 import com.jayway.jsonpath.ReadContext;
 import org.slf4j.Logger;
@@ -8,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,9 +23,14 @@ public class PlayerInformationParser {
     private ReadContext ctx;
 
     private Map<Long, Integer> platformIdToActorId = new HashMap<>();
+    private Map<Integer, TeamSide> actorIdToTeam = new HashMap<>();
+
+    private Map<Integer, MatchPlayerDTO> matchPlayers = new HashMap<>();
 
     void setUp() {
         platformIdToActorId = new HashMap<>();
+        actorIdToTeam = new HashMap<>();
+        matchPlayers = new HashMap<>();
     }
 
     void setCtx(ReadContext ctx) {
@@ -34,16 +42,69 @@ public class PlayerInformationParser {
         String actorUpdate = "].ActorUpdates[";
         String frame = "$.Frames[";
         if (ctx.read(frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['Engine.PlayerReplicationInfo:UniqueId']") != null) {
+            MatchPlayerDTO matchPlayer = new MatchPlayerDTO();
+            PlayerDTO player = new PlayerDTO();
+            matchPlayer.setPlayerDTO(player);
             Long uniqueId = ctx.read(frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['Engine.PlayerReplicationInfo:UniqueId'].SteamID64", Long.class);
             if (uniqueId != null) {
                 platformIdToActorId.put(uniqueId, actorId);
+                player.setPlatformID(uniqueId);
             } else {
                 uniqueId = ctx.read(frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['Engine.PlayerReplicationInfo:UniqueId'].PsnId", Long.class);
                 if (uniqueId != null) {
                     platformIdToActorId.put(uniqueId, actorId);
+                    player.setPlatformID(uniqueId);
                 } else {
                     throw new FileServiceException("Actor has no steam-id and no psn-id");
                 }
+            }
+            String nameJSON = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['Engine.PlayerReplicationInfo:PlayerName']";
+            String teamJSON = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['Engine.PlayerReplicationInfo:Team'].ActorId";
+            if (ctx.read(nameJSON) != null) {
+                player.setName(ctx.read(nameJSON, String.class));
+            } else {
+                throw new FileServiceException("Player has no name");
+            }
+            if (ctx.read(teamJSON) != null) {
+                matchPlayer.setTeamActorId(ctx.read(teamJSON, Integer.class));
+            } else {
+                throw new FileServiceException("Player has no team");
+            }
+            matchPlayer.setActorId(actorId);
+            matchPlayers.put(actorId, matchPlayer);
+        } else {
+            MatchPlayerDTO matchPlayer = matchPlayers.get(actorId);
+            String matchScore = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['TAGame.PRI_TA:MatchScore']";
+            String matchShots = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['TAGame.PRI_TA:MatchShots']";
+            String matchGoals = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['TAGame.PRI_TA:MatchGoals']";
+            String matchSaves = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['TAGame.PRI_TA:MatchSaves']";
+            String matchAssists = frame + currentFrame + actorUpdate + currentActorUpdateNr + "].['TAGame.PRI_TA:MatchAssists']";
+            if (ctx.read(matchScore) != null) {
+                matchPlayer.setScore(ctx.read(matchScore, Integer.class));
+            }
+            if (ctx.read(matchShots) != null) {
+                matchPlayer.setShots(ctx.read(matchShots, Integer.class));
+            }
+            if (ctx.read(matchGoals) != null) {
+                matchPlayer.setGoals(ctx.read(matchGoals, Integer.class));
+            }
+            if (ctx.read(matchSaves) != null) {
+                matchPlayer.setSaves(ctx.read(matchSaves, Integer.class));
+            }
+            if (ctx.read(matchAssists) != null) {
+                matchPlayer.setAssists(ctx.read(matchAssists, Integer.class));
+            }
+        }
+    }
+
+    void parseTeam(int actorId, int currentFrame, int currentActorUpdateNr) {
+        LOG.trace("called - parseTeam");
+        String teamArchetypes = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].TypeName", String.class);
+        if (teamArchetypes != null) {
+            if (teamArchetypes.equals("Archetypes.Teams.Team1")) {
+                actorIdToTeam.put(actorId, TeamSide.RED);
+            } else {
+                actorIdToTeam.put(actorId, TeamSide.BLUE);
             }
         }
     }
@@ -57,6 +118,15 @@ public class PlayerInformationParser {
                 throw new FileServiceException("No ActorId for a player");
             }
         }
+    }
+
+    ArrayList<MatchPlayerDTO> getMatchPlayer() {
+        ArrayList<MatchPlayerDTO> list = new ArrayList<>();
+        for (MatchPlayerDTO matchPlayerDTO : matchPlayers.values()) {
+            matchPlayerDTO.setTeam(actorIdToTeam.get(matchPlayerDTO.getTeamActorId()));
+            list.add(matchPlayerDTO);
+        }
+        return list;
     }
 
     public Map<Long, Integer> getPlatformIdToActorId() {
