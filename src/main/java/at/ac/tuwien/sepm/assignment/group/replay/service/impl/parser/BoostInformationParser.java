691c8700fd25c35c4a3abc76c3059da4e0c9d32a
Elias Brugger <eliasbrugger03gmail.com>
2019-01-24 15:12:26 +0100

    Finishing refactoring of the boost information parser. [issue #32626]

--- src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/BoostInformationParser.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/BoostInformationParser.java
@@ -15,16 +15,12 @@ import java.util.*;
 
 @Service
 public class BoostInformationParser {
-    private int actorId;
-    private int currentFrame;
-    private int currentActorUpdateNr;
-    private double frameTime;
-    private double frameDelta;
-    private boolean gamePaused;
-    private int actorUpdateCount;
 
     private ReadContext ctx;
     private CarInformationParser carInformationParser;
+
+    private static final String FRAMESTRING = "$.Frames[";
+    private static final String ACTORUPDATESTRING = "].ActorUpdates[";
 
     //Logger
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -37,21 +33,20 @@ public class BoostInformationParser {
         this.carInformationParser = carInformationParser;
     }
 
-    //Retrieved from CarInformationParser
-    private Map<Integer, Integer> carPlayerMap;
-
+    //Map that maps car component to car id. Key = componentId, Value = carActorId
     private Map<Integer, Integer> carComponentToCarId;
-
     //Map that maps ActorID from a Car to a list of BoostAmountInformation. Key = playerId, Value = Boost amount information
     private Map<Integer, List<BoostDTO>> boostAmountMap;
     //Map that maps ActorID from a Car to a list of BoostPadDTO. Key = playerId, Value = Boost pad information
     private Map<Integer, Map<Integer, List<BoostPadDTO>>> boostPadMap;
 
+    /**
+     * Setup the maps each time a replay gets uploaded
+     */
     void setup(){
         carComponentToCarId = new HashMap<>();
         boostAmountMap = new HashMap<>();
         boostPadMap = new HashMap<>();
-        carPlayerMap = carInformationParser.getPlayerCarMap();
     }
 
     /**
@@ -66,17 +61,9 @@ public class BoostInformationParser {
      */
     void parse(int actorId, int currentFrame, int currentActorUpdateNr, double frameTime, double frameDelta, boolean gamePaused) throws FileServiceException {
 
-
-        this.actorId = actorId;
-        this.currentFrame = currentFrame;
-        this.currentActorUpdateNr = currentActorUpdateNr;
-        this.frameTime = frameTime;
-        this.frameDelta = frameDelta;
-        this.gamePaused = gamePaused;
-
-        getCarIDFromCarComponent();
+        getCarIDFromCarComponent(currentFrame, currentActorUpdateNr);
         //parse the amount of boost, check if the classname is not 'TAGame.CarComponent_TA:ReplicatedActive' since this indicates active boosting, will be done later.
-        parseBoostAmountInformation();
+        parseBoostAmountInformation(actorId, currentFrame, currentActorUpdateNr, frameTime, frameDelta, gamePaused);
 
     }
 
@@ -91,26 +78,18 @@ public class BoostInformationParser {
      * @throws FileServiceException if the file couldn't be parsed
      */
     void parseBoostPad(int actorId, int currentFrame, int currentActorUpdateNr, double frameTime, double frameDelta, boolean gamePaused, int actorUpdateCount) throws FileServiceException {
-        this.actorId = actorId;
-        this.currentFrame = currentFrame;
-        this.currentActorUpdateNr = currentActorUpdateNr;
-        this.frameTime = frameTime;
-        this.frameDelta = frameDelta;
-        this.gamePaused = gamePaused;
-        this.actorUpdateCount = actorUpdateCount;
-
         //parse the boost pads information, if big or little pad was picked up and where.
-        parseBoostPadInformation();
+        parseBoostPadInformation(currentFrame, currentActorUpdateNr, actorUpdateCount, frameTime, frameDelta, gamePaused);
     }
 
     /**
      * Retrieves the car ID from the boost component and saves it in a map.
      */
-    private void getCarIDFromCarComponent() {
+    private void getCarIDFromCarComponent(int currentFrame, int currentActorUpdateNr) {
         LOG.trace("Called - getCarIDfromBoost");
         try {
-            int carComponentId = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].Id", Integer.class);
-            int carActorId = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.CarComponent_TA:Vehicle'].ActorId", Integer.class);
+            int carComponentId = ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].Id", Integer.class);
+            int carActorId = ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].['TAGame.CarComponent_TA:Vehicle'].ActorId", Integer.class);
             carComponentToCarId.putIfAbsent(carComponentId, carActorId);
         } catch (PathNotFoundException e) {
             // ignore
@@ -121,12 +100,12 @@ public class BoostInformationParser {
      * Parses the amount of boost for a player to a specific frame
      *
      */
-    private void parseBoostAmountInformation() {
+    private void parseBoostAmountInformation(int actorId, int currentFrame, int currentActorUpdateNr, double frameTime, double frameDelta, boolean gamePaused) {
         LOG.trace("Called - parseBoostAmountInformation");
 
         try {
-            if(ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.CarComponent_Boost_TA:ReplicatedBoostAmount']", Integer.class) != null) {
-                int currentBoost = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.CarComponent_Boost_TA:ReplicatedBoostAmount']", Integer.class);
+            if(ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].['TAGame.CarComponent_Boost_TA:ReplicatedBoostAmount']", Integer.class) != null) {
+                int currentBoost = ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].['TAGame.CarComponent_Boost_TA:ReplicatedBoostAmount']", Integer.class);
                 //map to 0 - 100, in the json it is from 0 - 255
                 currentBoost = (int) (currentBoost / 255.0 * 100.0);
                 //create new boost object
@@ -147,17 +126,17 @@ public class BoostInformationParser {
      * Parses the boost pad informations
      *
      */
-    private void parseBoostPadInformation() {
+    private void parseBoostPadInformation(int currentFrame, int currentActorUpdateNr, int actorUpdateCount, double frameTime, double frameDelta, boolean gamePaused) {
         LOG.trace("Called - parseBoostPadInformation");
 
         //create new boost pad object, little boost pad = 12, big boost pad = 100 boost.
         try {
             BoostPadDTO boostPad = new BoostPadDTO(frameTime, frameDelta, currentFrame, gamePaused);
 
-            if(ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.VehiclePickup_TA:ReplicatedPickupData'].ActorId", Integer.class) != null) {
-                int carID = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.VehiclePickup_TA:ReplicatedPickupData'].ActorId", Integer.class);
+            if(ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].['TAGame.VehiclePickup_TA:ReplicatedPickupData'].ActorId", Integer.class) != null) {
+                int carID = ctx.read(FRAMESTRING + currentFrame + ACTORUPDATESTRING + currentActorUpdateNr + "].['TAGame.VehiclePickup_TA:ReplicatedPickupData'].ActorId", Integer.class);
 
-                int id = getPickedUpPosition(carID);
+                int id = getPickedUpPosition(carID, currentFrame, actorUpdateCount);
 
                 // id == -1 --> no position found in the surrounding frames
                 if(id != -1) {
@@ -171,7 +150,14 @@ public class BoostInformationParser {
         }
     }
 
-    private int getPickedUpPosition(int carID) {
+    /**
+     * Evaluates the position where the boost pad was picked up
+     * @param carID the car that picked up the boost pad
+     * @param currentFrame the current frame where the event has happened
+     * @param actorUpdateCount number of actor updates for the current frame
+     * @return the id of the boostpad
+     */
+    private int getPickedUpPosition(int carID, int currentFrame, int actorUpdateCount) {
         //loop through all current updates and search for a position for the specific player to evaulate which boost pad was picked up
         String actorUpdatesLength = "].ActorUpdates.length()";
         int id = -1;
@@ -186,7 +172,7 @@ public class BoostInformationParser {
             }
 
             // search position in the n-1th frame
-            int actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame - 1) + actorUpdatesLength);
+            int actorUpdateCount2 = ctx.read(FRAMESTRING + (currentFrame - 1) + actorUpdatesLength);
             for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
                 id = searchSurroundingFrames(currentFrame, -1, currentActorUpdate, carID);
             }
@@ -196,7 +182,7 @@ public class BoostInformationParser {
             }
 
             // search position in the n+1th frame
-            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame + 1) + actorUpdatesLength);
+            actorUpdateCount2 = ctx.read(FRAMESTRING + (currentFrame + 1) + actorUpdatesLength);
             for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
                 id = searchSurroundingFrames(currentFrame, 1, currentActorUpdate, carID);
             }
@@ -206,7 +192,7 @@ public class BoostInformationParser {
             }
 
             // search position in the n-2th frame
-            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame - 2) + actorUpdatesLength);
+            actorUpdateCount2 = ctx.read(FRAMESTRING + (currentFrame - 2) + actorUpdatesLength);
             for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
                 id = searchSurroundingFrames(currentFrame, -2, currentActorUpdate, carID);
             }
@@ -216,7 +202,7 @@ public class BoostInformationParser {
             }
 
             // search position in the n+2th frame
-            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame + 2) + actorUpdatesLength);
+            actorUpdateCount2 = ctx.read(FRAMESTRING + (currentFrame + 2) + actorUpdatesLength);
             for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
                 id = searchSurroundingFrames(currentFrame, 2, currentActorUpdate, carID);
             }
@@ -230,24 +216,37 @@ public class BoostInformationParser {
          return -1;
     }
 
+    /**
+     * Searches for a position of the player that picked up the boost in the given frame
+     * @param currentFrame current frame
+     * @param frameOffset frame offset
+     * @param currentActorUpdate current actor update
+     * @param carID car ID to search for
+     * @return the id of the boost pad
+     */
     private int searchSurroundingFrames(int currentFrame, int frameOffset, int currentActorUpdate, int carID) {
         try {
-            //if (ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-            int actID = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
+            int actID = ctx.read(FRAMESTRING + (currentFrame + frameOffset) + ACTORUPDATESTRING + currentActorUpdate + "].Id", Integer.class);
 
             if (actID == carID) {
-                float x = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                float y = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                float z = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
+                float x = ctx.read(FRAMESTRING + (currentFrame + frameOffset) + ACTORUPDATESTRING + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
+                float y = ctx.read(FRAMESTRING + (currentFrame + frameOffset) + ACTORUPDATESTRING + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
+                float z = ctx.read(FRAMESTRING + (currentFrame + frameOffset) + ACTORUPDATESTRING + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
                 return getIDFromPosition(x, y, z);
             }
-            //}
         } catch (PathNotFoundException e) {
             // ignore
         }
         return -1;
     }
 
+    /**
+     * Retrieves the id from the position
+     * @param x x value
+     * @param y y value
+     * @param z z value
+     * @return the id of the boost pad
+     */
     private int getIDFromPosition(float x, float y, float z) {
         return BoostIDs.getID(x, y, z);
     }
@@ -258,6 +257,10 @@ public class BoostInformationParser {
         }
     }
 
+    /**
+     * Gets the boost amount map
+     * @return boost amount map
+     */
     public Map<Integer, List<BoostDTO>> getBoostAmountMap() {
         LOG.trace("Called - boost amount calculate");
         Map<Integer, List<BoostDTO>> boostAmounts = new HashMap<>();
@@ -275,7 +278,11 @@ public class BoostInformationParser {
         return boostAmounts;
     }
 
-    public Map<Integer, Map<Integer, List<BoostPadDTO>>> getBoostPadMap() {
+    /**
+     * Gets the boost pad map
+     * @return boost pad map
+     */
+    Map<Integer, Map<Integer, List<BoostPadDTO>>> getBoostPadMap() {
         // swap carID as keys and playerID as keys.
         LOG.trace("Called - boost pads calculate");
         Map<Integer, Map<Integer, List<BoostPadDTO>>> padMap = new HashMap<>();
