0b75958042c254d793a20c02dc206387fc2f88e4
Elias Brugger <eliasbrugger03gmail.com>
2019-01-24 14:05:21 +0100

    Did some more refactoring to the boost information parser to avoid duplicate code. [issue #32626]

--- src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/BoostInformationParser.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/group/replay/service/impl/parser/BoostInformationParser.java
@@ -4,12 +4,8 @@ import at.ac.tuwien.sepm.assignment.group.replay.dto.BoostIDs;
 import at.ac.tuwien.sepm.assignment.group.replay.dto.BoostPadDTO;
 import at.ac.tuwien.sepm.assignment.group.replay.service.exception.FileServiceException;
 import at.ac.tuwien.sepm.assignment.group.replay.dto.BoostDTO;
-import at.ac.tuwien.sepm.assignment.group.replay.service.impl.RigidBodyInformation;
 import com.jayway.jsonpath.PathNotFoundException;
 import com.jayway.jsonpath.ReadContext;
-import javafx.collections.transformation.SortedList;
-import net.minidev.json.JSONArray;
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -32,7 +28,6 @@ public class BoostInformationParser {
 
     //Logger
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private int counter = 0;
 
     void setCtx(ReadContext ctx) {
         this.ctx = ctx;
@@ -43,7 +38,7 @@ public class BoostInformationParser {
     }
 
     //Retrieved from CarInformationParser
-    Map<Integer, Integer> carPlayerMap;
+    private Map<Integer, Integer> carPlayerMap;
 
     private Map<Integer, Integer> carComponentToCarId;
 
@@ -52,14 +47,11 @@ public class BoostInformationParser {
     //Map that maps ActorID from a Car to a list of BoostPadDTO. Key = playerId, Value = Boost pad information
     private Map<Integer, Map<Integer, List<BoostPadDTO>>> boostPadMap;
 
-    private List<Integer> uniqueBoostPadIds = new LinkedList<>();
-
     void setup(){
         carComponentToCarId = new HashMap<>();
         boostAmountMap = new HashMap<>();
         boostPadMap = new HashMap<>();
         carPlayerMap = carInformationParser.getPlayerCarMap();
-        uniqueBoostPadIds = new LinkedList<>();
     }
 
     /**
@@ -121,7 +113,7 @@ public class BoostInformationParser {
             int carActorId = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdateNr + "].['TAGame.CarComponent_TA:Vehicle'].ActorId", Integer.class);
             carComponentToCarId.putIfAbsent(carComponentId, carActorId);
         } catch (PathNotFoundException e) {
-            LOG.debug("No Information about boost found");
+            // ignore
         }
     }
 
@@ -146,7 +138,7 @@ public class BoostInformationParser {
                 }
             }
         } catch (PathNotFoundException e) {
-            LOG.debug("No Information about boost amount found");
+            // ignore
         }
 
     }
@@ -175,140 +167,95 @@ public class BoostInformationParser {
                 }
             }
         } catch (PathNotFoundException e) {
-            LOG.debug("No Information about boost pad found");
+            // ignore
         }
     }
 
     private int getPickedUpPosition(int carID) {
         //loop through all current updates and search for a position for the specific player to evaulate which boost pad was picked up
-        double x;
-        double y;
-        double z;
-        int actID = -1;
+        String actorUpdatesLength = "].ActorUpdates.length()";
+        int id = -1;
         try {
             // search position in the current frame
             for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount; currentActorUpdate++) {
-                if (ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-                    actID = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
-
-                    if (actID == carID) {
-                        x = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                        y = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                        z = ctx.read("$.Frames[" + currentFrame + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
-                        return getIDFromPosition(x, y, z);
-                    }
-                }
-
+                id = searchSurroundingFrames(currentFrame, 0, currentActorUpdate, carID);
             }
-            // search position in the n-1th frame
-            int actorUpdateCount_ = ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates.length()");
-            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount_; currentActorUpdate++) {
-                if (ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-                    actID = ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
-
-                    if (actID == carID) {
-                        x = ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                        y = ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                        z = ctx.read("$.Frames[" + (currentFrame - 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
-                        return getIDFromPosition(x, y, z);
-                    }
-                }
-
-            }
-
-            // search position in the n+1th frame
-            actorUpdateCount_ = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates.length()");
-            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount_; currentActorUpdate++) {
-                if (ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-                    actID = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
-
-                    if (actID == carID) {
-                        x = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                        y = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                        z = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
-                        return getIDFromPosition(x, y, z);
-                    }
-                }
-
+            // return the id if a position was found in the surrounding frames
+            if(id != -1) {
+                return id;
             }
 
             // search position in the n-1th frame
-            actorUpdateCount_ = ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates.length()");
-            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount_; currentActorUpdate++) {
-                if (ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-                    actID = ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
-
-                    if (actID == carID) {
-                        x = ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                        y = ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                        z = ctx.read("$.Frames[" + (currentFrame - 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
-                        return getIDFromPosition(x, y, z);
-                    }
-                }
-
+            int actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame - 1) + actorUpdatesLength);
+            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
+                id = searchSurroundingFrames(currentFrame, -1, currentActorUpdate, carID);
+            }
+            // return the id if a position was found in the surrounding frames
+            if(id != -1) {
+                return id;
             }
 
             // search position in the n+1th frame
-            actorUpdateCount_ = ctx.read("$.Frames[" + (currentFrame + 2) + "].ActorUpdates.length()");
-            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount_; currentActorUpdate++) {
-                if (ctx.read("$.Frames[" + (currentFrame + 2) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
-                    actID = ctx.read("$.Frames[" + (currentFrame + 1) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
+            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame + 1) + actorUpdatesLength);
+            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
+                id = searchSurroundingFrames(currentFrame, 1, currentActorUpdate, carID);
+            }
+            // return the id if a position was found in the surrounding frames
+            if(id != -1) {
+                return id;
+            }
 
-                    if (actID == carID) {
-                        x = ctx.read("$.Frames[" + (currentFrame + 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
-                        y = ctx.read("$.Frames[" + (currentFrame + 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
-                        z = ctx.read("$.Frames[" + (currentFrame + 2) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
-                        return getIDFromPosition(x, y, z);
-                    }
-                }
+            // search position in the n-2th frame
+            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame - 2) + actorUpdatesLength);
+            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
+                id = searchSurroundingFrames(currentFrame, -2, currentActorUpdate, carID);
+            }
+            // return the id if a position was found in the surrounding frames
+            if(id != -1) {
+                return id;
+            }
 
+            // search position in the n+2th frame
+            actorUpdateCount2 = ctx.read("$.Frames[" + (currentFrame + 2) + actorUpdatesLength);
+            for (int currentActorUpdate = 0; currentActorUpdate < actorUpdateCount2; currentActorUpdate++) {
+                id = searchSurroundingFrames(currentFrame, 2, currentActorUpdate, carID);
+            }
+            // return the id if a position was found in the surrounding frames
+            if(id != -1) {
+                return id;
             }
         } catch (PathNotFoundException e) {
-            LOG.debug("No Information about position found");
-            return -1;
+            // ignore
         }
          return -1;
     }
 
-    private int getIDFromPosition(double x, double y, double z) {
+    private int searchSurroundingFrames(int currentFrame, int frameOffset, int currentActorUpdate, int carID) {
+        try {
+            //if (ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class) != null) {
+            int actID = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].Id", Integer.class);
+
+            if (actID == carID) {
+                float x = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.X", Float.class);
+                float y = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Y", Float.class);
+                float z = ctx.read("$.Frames[" + (currentFrame + frameOffset) + "].ActorUpdates[" + currentActorUpdate + "].['TAGame.RBActor_TA:ReplicatedRBState'].Position.Z", Float.class);
+                return getIDFromPosition(x, y, z);
+            }
+            //}
+        } catch (PathNotFoundException e) {
+            // ignore
+        }
+        return -1;
+    }
+
+    private int getIDFromPosition(float x, float y, float z) {
         return BoostIDs.getID(x, y, z);
     }
 
     private void fillBoostPadIds(int actId) {
-        boostPadMap.get(actId).putIfAbsent(0, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(1, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(2, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(3, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(4, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(5, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(6, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(7, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(8, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(9, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(10, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(11, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(12, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(13, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(14, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(15, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(16, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(17, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(18, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(19, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(20, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(21, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(22, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(23, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(24, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(25, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(26, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(27, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(28, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(29, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(30, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(31, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(32, new LinkedList<>());
-        boostPadMap.get(actId).putIfAbsent(33, new LinkedList<>());
+        for(int i=0; i<=33; i++) {
+            boostPadMap.get(actId).putIfAbsent(i, new LinkedList<>());
+        }
     }
 
     public Map<Integer, List<BoostDTO>> getBoostAmountMap() {
