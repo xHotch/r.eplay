03a96e566e1f30c03e1b17f0350a246018605d58
Markus Kogelbauer <markus.kogelbauer@hotmail.com>
2019-01-26 21:00:15 +0100

    Added Tests to check Persistence exceptions in the JDBCDAOs [#32389]

--- src/test/java/at/ac/tuwien/sepm/assignment/group/replay/MatchDAOTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/group/replay/MatchDAOTest.java
@@ -170,6 +170,12 @@ public class MatchDAOTest {
 
     }
 
+    @Test(expected = MatchPersistenceException.class)
+    public void readMatchesWithoutConnectionShouldThrowException() throws MatchPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        matchDAO.readMatches();
+    }
+
     @Test
     public void createMatchTest() throws MatchPersistenceException, MatchAlreadyExistsException {
         MatchDTO matchDTO = new MatchDTO();
@@ -233,6 +239,12 @@ public class MatchDAOTest {
         Assert.assertThat(matchPlayers.size(), is(2));
     }
 
+    @Test(expected = MatchPersistenceException.class)
+    public void createMatchWithoutConnectionShouldThrowException() throws MatchPersistenceException, MatchAlreadyExistsException {
+        jdbcConnectionManager.closeConnection();
+        matchDAO.createMatch(new MatchDTO());
+    }
+
     @Test(expected = MatchAlreadyExistsException.class)
     public void createMatchTestShouldThrowMatchAlreadyExistsException() throws MatchPersistenceException, MatchAlreadyExistsException {
         MatchDTO matchDTO = new MatchDTO();
@@ -246,6 +258,14 @@ public class MatchDAOTest {
         matchDTO.setId(1);
         matchDAO.deleteMatch(matchDTO);
         Assert.assertThat(matchDAO.readMatches().size(), is(1));
+    }
+
+    @Test(expected = MatchPersistenceException.class)
+    public void deleteMatchWithoutConnectionShouldThrowException() throws MatchPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        MatchDTO matchDTO = new MatchDTO();
+        matchDTO.setId(1);
+        matchDAO.deleteMatch(matchDTO);
     }
 
     @Test
@@ -272,6 +292,12 @@ public class MatchDAOTest {
         Assert.assertThat(retrievedMatches.size(), is(0));
     }
 
+    @Test(expected = MatchPersistenceException.class)
+    public void searchMatchesWithoutConnectionShouldThrowException() throws MatchPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        matchDAO.searchMatches("", null, null, 0);
+    }
+
     @Test
     public void readMatchesFromPlayerTest() throws MatchPersistenceException {
         PlayerDTO player = new PlayerDTO();
@@ -284,5 +310,13 @@ public class MatchDAOTest {
 
         retrievedMatches = matchDAO.readMatchesFromPlayer(player);
         Assert.assertThat(retrievedMatches.size(), is(0));
+    }
+
+    @Test(expected = MatchPersistenceException.class)
+    public void readMatchesFromPlayerWithoutConnectionShouldThrowException() throws MatchPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        PlayerDTO player = new PlayerDTO();
+        player.setId(1);
+        matchDAO.readMatchesFromPlayer(player);
     }
 }
--- src/test/java/at/ac/tuwien/sepm/assignment/group/replay/PlayerDAOTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/group/replay/PlayerDAOTest.java
@@ -93,6 +93,12 @@ public class PlayerDAOTest {
         Assert.assertTrue(player1.isShown());
     }
 
+    @Test(expected = PlayerPersistenceException.class)
+    public void readPlayersWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        playerDAO.readPlayers();
+    }
+
     @Test
     public void createPlayerTest() throws PlayerPersistenceException {
         PlayerDTO player = new PlayerDTO();
@@ -111,6 +117,16 @@ public class PlayerDAOTest {
         Assert.assertTrue(readPlayer.isShown());
     }
 
+    @Test(expected = PlayerPersistenceException.class)
+    public void createPlayerWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        PlayerDTO player = new PlayerDTO();
+        player.setPlatformID(123L);
+        player.setName("Test Player");
+        player.setShown(true);
+        playerDAO.createPlayer(player);
+    }
+
     @Test
     public void deletePlayerTest() throws PlayerPersistenceException {
         PlayerDTO player = new PlayerDTO();
@@ -118,6 +134,14 @@ public class PlayerDAOTest {
         playerDAO.deletePlayer(player);
         List<PlayerDTO> players = playerDAO.readPlayers();
         Assert.assertThat(players.size(), is(3));
+    }
+
+    @Test(expected = PlayerPersistenceException.class)
+    public void deletePlayerWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        PlayerDTO player = new PlayerDTO();
+        player.setId(1L);
+        playerDAO.deletePlayer(player);
     }
 
     @Test
@@ -129,6 +153,14 @@ public class PlayerDAOTest {
         Assert.assertThat(players.size(), is(5));
     }
 
+    @Test(expected = PlayerPersistenceException.class)
+    public void showPlayerWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        PlayerDTO player = new PlayerDTO();
+        player.setId(5L);
+        playerDAO.showPlayer(player);
+    }
+
     @Test
     public void getTest() throws PlayerPersistenceException {
         PlayerDTO player = playerDAO.get(1);
@@ -136,6 +168,12 @@ public class PlayerDAOTest {
         Assert.assertThat(player.getId(), is(1L));
         Assert.assertThat(player.getPlatformID(), is(123456L));
         Assert.assertTrue(player.isShown());
+    }
+
+    @Test(expected = PlayerPersistenceException.class)
+    public void getWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        playerDAO.get(1);
     }
 
     @Test
@@ -150,6 +188,15 @@ public class PlayerDAOTest {
         Assert.assertThat(avgStatsDTO.getSaves(), is(0.0));
         Assert.assertThat(avgStatsDTO.getGoals(), is(2.0));
         Assert.assertThat(avgStatsDTO.getShots(), is(3.5));
+    }
+
+    @Test(expected = PlayerPersistenceException.class)
+    public void getAvgStatsWithoutConnectionShouldThrowException() throws PlayerPersistenceException {
+        jdbcConnectionManager.closeConnection();
+        PlayerDTO player = new PlayerDTO();
+        player.setId(1L);
+        MatchType matchType = MatchType.RANKED2V2;
+        playerDAO.getAvgStats(player, matchType);
     }
 
 
